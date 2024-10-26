adf6bee752bea00fe543dd59b74aa2837d781ab1
Froger <hellofrogworld@gmail.com>
2024-10-26 11:39:43 -0700

    Create question.cpp

---
+++ question.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string x;
+  cout << "The answer is yes"<<endl; 
+  cin >> x; 
+  if (x == "yes") {
+  cout << "correct\n";
+  }else {
+  cout << "YOU ARE SO BAD AT THIS";
+  }
+  
+  return 0;
+}
